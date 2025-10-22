7a3bf1e6479fdb60b2862220d8b7c613de39db23
Adam Sasine <adam.sasine@gmail.com>
2025-10-22 15:26:26 -0700

    Inline format args

--- examples/std/src/lib/type_c/mock_controller.rs
+++ examples/std/src/lib/type_c/mock_controller.rs
@@ -182,10 +182,10 @@ impl embedded_services::type_c::controller::Controller for Controller<'_> {
     async fn get_pd_alert(&mut self, port: LocalPortId) -> Result<Option<Ado>, Error<Self::BusError>> {
         let pd_alert = self.state.pd_alert.lock().await;
         if let Some(ado) = *pd_alert {
-            debug!("{:?}: Get PD alert: {ado:#?}", port);
+            debug!("{port:?}: Get PD alert: {ado:#?}");
             Ok(Some(ado))
         } else {
-            debug!("{:?}: No PD alert", port);
+            debug!("{port:?}: No PD alert");
             Ok(None)
         }
     }
@@ -224,7 +224,7 @@ impl embedded_services::type_c::controller::Controller for Controller<'_> {
         port: LocalPortId,
         voltage_mv: Option<u16>,
     ) -> Result<(), Error<Self::BusError>> {
-        debug!("Set max sink voltage for {:?}: {:?}", port, voltage_mv);
+        debug!("Set max sink voltage for {port:?}: {voltage_mv:?}");
         Ok(())
     }
 
