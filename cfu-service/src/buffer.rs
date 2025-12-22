1d2e7960c699125a8853796adabe1d13322fe50e
Jerry Xie <jerryxie@microsoft.com>
2025-12-22 15:43:34 -0600

    cfu-service: audit panic paths

--- cfu-service/src/buffer.rs
+++ cfu-service/src/buffer.rs
@@ -234,6 +234,8 @@ impl<'a> Buffer<'a> {
     ///
     /// If the component is busy, this will wait indefinitely since the component will not be able to process
     async fn wait_buffered_content(&self, is_busy: bool) -> FwUpdateContentCommand {
+        // panic safety: this branch is only taken if the component is busy, so this future will never complete
+        #[allow(clippy::unreachable)]
         if is_busy {
             let () = pending().await;
             unreachable!();
