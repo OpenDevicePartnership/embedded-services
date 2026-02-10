6d6f7792f05877328be2f74a2ed0e4a09ad5ed5e
Robert Zieba <robertzieba@microsoft.com>
2026-02-09 16:57:32 -0800

    Move power policy and type-C code out of `embedded-services`

--- battery-service/src/lib.rs
+++ battery-service/src/lib.rs
@@ -126,7 +126,9 @@ impl comms::MailboxDelegate for Service {
             self.context.send_event_no_wait(*event).map_err(|e| match e {
                 embassy_sync::channel::TrySendError::Full(_) => comms::MailboxDelegateError::BufferFull,
             })?
-        } else if let Some(power_policy_msg) = message.data.get::<embedded_services::power::policy::CommsMessage>() {
+        } else if let Some(battery_request) = message.data.get::<AcpiBatteryRequest>() {
+            self.context.send_acpi_cmd(*battery_request);
+        } else if let Some(power_policy_msg) = message.data.get::<power_policy_service::policy::CommsMessage>() {
             self.context.set_power_info(&power_policy_msg.data)?;
         }
 
