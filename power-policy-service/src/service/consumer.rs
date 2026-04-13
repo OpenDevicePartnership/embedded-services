efa225632b82e4aface7ecc40c5656710c664147
Billy Price <williamp@microsoft.com>
2026-04-13 15:46:18 -0700

    clippy

--- power-policy-service/src/service/consumer.rs
+++ power-policy-service/src/service/consumer.rs
@@ -102,11 +102,10 @@ impl<'device, Reg: Registration<'device>> Service<'device, Reg> {
         // Count how many available unconstrained devices we have
         let mut unconstrained_new = UnconstrainedState::default();
         for psu in self.registration.psus() {
-            if let Some(capability) = psu.lock().await.state().consumer_capability {
-                if capability.flags.unconstrained_power() {
+            if let Some(capability) = psu.lock().await.state().consumer_capability
+                && capability.flags.unconstrained_power() {
                     unconstrained_new.available += 1;
                 }
-            }
         }
 
         // The overall unconstrained state is true if an unconstrained consumer is currently connected
