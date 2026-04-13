950d35ed71e8c5643f0bce1258b17778a40909a0
Ayodeji Ige <ayodeji.ige@microsoft.com>
2026-04-13 14:22:22 -0700

    Fixed cargo test compilation by refactoring dependencies and features in Cargo.toml files for debug-service, espi-service, and examples

--- debug-service/src/lib.rs
+++ debug-service/src/lib.rs
@@ -3,8 +3,12 @@
 #![allow(clippy::indexing_slicing)]
 #![allow(clippy::unwrap_used)]
 
+#[cfg(feature = "defmt")]
 mod debug_service;
+#[cfg(feature = "defmt")]
 mod defmt_ring_logger;
+#[cfg(feature = "defmt")]
 pub mod task;
 
+#[cfg(feature = "defmt")]
 pub use debug_service::*;
--- espi-service/src/lib.rs
+++ espi-service/src/lib.rs
@@ -4,7 +4,10 @@
 #![allow(clippy::panic)]
 #![allow(clippy::unwrap_used)]
 
+#[cfg(feature = "imxrt")]
 mod espi_service;
+#[cfg(feature = "imxrt")]
 pub mod task;
 
+#[cfg(feature = "imxrt")]
 pub use espi_service::*;
